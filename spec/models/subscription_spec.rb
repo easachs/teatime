d28ad64ca39cbb2565fa71fcdd0c802af1d16449
Eli Sachs <easachs13@gmail.com>
2022-10-31 15:20:57 -0600

    Test: create model specs

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Subscription do
+  describe 'relations' do
+    it { should belong_to(:tea) }
+    it { should belong_to(:customer) }
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :price }
+  end
+end
---
+++ spec/models/tea_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Tea do
+  describe 'relations' do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:customers).through(:subscriptions) }
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+  end
+end
