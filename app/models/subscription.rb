124ae7f43812cff743f4db3b77c2bb67baf9d680
Eli Sachs <easachs13@gmail.com>
2022-11-01 10:30:15 -0600

    Feat: create models/db relations

---
+++ app/models/subscription.rb
@@ -0,0 +1,5 @@
+class Subscription < ApplicationRecord
+  belongs_to :tea
+  belongs_to :customer
+  validates_presence_of :title, :price
+end
---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Customer do
+  describe 'relations' do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:teas).through(:subscriptions) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :address }
+  end
+end
