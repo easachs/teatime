788a0a89687cf8803f8f5c698836029fa8109d4b
Eli Sachs <easachs13@gmail.com>
2022-11-01 10:34:19 -0600

    Feat: update customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,6 @@
 class Customer < ApplicationRecord
   has_many :subscriptions
   has_many :teas, through: :subscriptions
-  validates_presence_of :email, :address
+  validates_presence_of :first_name, :last_name, :address
+  validates :email, presence: true, uniqueness: true
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Customer do
   end
 
   describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
     it { should validate_presence_of :email }
     it { should validate_presence_of :address }
   end
